a74207345c1ac5186db6df1a16e88dd767548edc
Chloe <cfontaine@citycable.ch>
2021-01-08 10:06:13 +0100

    pouf

--- date.cpp
+++ date.cpp
@@ -14,8 +14,9 @@ Compilateur     : Mingw-w64 g++ 8.1.0
 -----------------------------------------------------------------------------------
 */
 
-#include "date.h"
 #include <iomanip>
+#include <sstream>
+#include "date.h"
 
 using namespace std;
 
